9b9496f4f69bf34f4b7f08ac9b1b271298bdc488
jmarkos <markos@joomlashack.com>
2013-10-29 19:13:21 -0600

    adding structure for single article

---
+++ code/html/com_content/article/default.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Tripod
+ * @subpackage  Overrider
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Joomlashack. Meritage Assets.  All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+
+$template = $app->getTemplate(true);
+
+	$this->wrightElementsStructure = Array(
+		'image',
+		'icons',
+		'title',
+		'article-info',
+		'content'
+
+	);
+
+$this->wrightExtraClass = "overlapped";
+
+require_once(JPATH_THEMES.'/'.$app->getTemplate().'/'.'wright'.'/'.'html'.'/'.'overrider.php');
+include(Overrider::getOverride('com_content.article'));
+?>
