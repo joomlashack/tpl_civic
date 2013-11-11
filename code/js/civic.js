fa3e5251ffcaa4501b80cd7474f502a53468f9fe
jmarkos <markos@joomlashack.com>
2013-11-11 16:24:50 -0600

    removing  method css of civic.js

--- code/js/civic.js
+++ code/js/civic.js
@@ -5,9 +5,9 @@
 jQuery(function() {
 	function CivciBgRezise(el){
 		el.css({
-			'background-image' : 'url(' + jQuery('.grid-top3').attr('data-bg-grid-top')+ ')',
-			'filter': 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src= "' + jQuery('.grid-top3').attr('data-bg-grid-top') + '",sizingMethod="scale")'
+			'background-image' : 'url(' + el.attr('data-bg-grid-top')+ ')'
 		});
 	}
+	CivciBgRezise(jQuery('.grid-bottom3'));
 	CivciBgRezise(jQuery('.grid-top3'));
 });
