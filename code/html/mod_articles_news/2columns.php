0cdc33dee1fa17a11f830d7bbdea8ab1f4c1641d
José Markos <markos@joomlashack.com>
2013-11-27 16:06:23 -0600

    Fixing row fluid close in last item of newsflash module 2columns layout

--- code/html/mod_articles_news/2columns.php
+++ code/html/mod_articles_news/2columns.php
@@ -34,7 +34,7 @@ defined('_JEXEC') or die;
 			<?php endif; ?>
 
 		</div>
-		<?php if ($cont % 2 !=  0): ?>
+		<?php if ($cont % 2 !=  0 || $cont == $n - 1): ?>
 			</div>
 		<?php endif; ?>
 		<?php
